495056d7b3901cd725da3f9c16e0041fe529dffc
cursomg160 <cursomg160@mtin.es>
2018-05-10 10:02:31 +0200

    Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /*
+   * Se pone un comentario para quitar el TODO que había antes.
+   * @param someone Se indica un valor
+   * @return devuelve la cadena Hello, con el nombre dado en el parámetro someone
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
